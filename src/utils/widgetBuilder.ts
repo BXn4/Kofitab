3e7de00d834b6b8a69c8cc393cad5f139b93f3e5
BXn4 <bencegamer90@gmail.com>
2024-11-23 15:29:43 +0100

    Added widget category

--- src/utils/widgetBuilder.ts
+++ src/utils/widgetBuilder.ts
@@ -3,36 +3,51 @@ import { TimeWidget } from "./widgets/time";
 import { DateWidget } from "./widgets/date";
 import { SearchWidget } from "./widgets/search";
 
-enum WidgetType {
-    Text = "Text",
-    Time = "Time",
-    Date = "Date",
-    Search = "Search"
-};
-
 interface Widget {
     type: WidgetType;
+    category: WidgetCategory;
     className: string;
     content: string;
 };
 
-function createWidgets(type: WidgetType): string {
-    switch (type) {
-        case WidgetType.Text:
-            return TextWidget;
-        case WidgetType.Time:
-            return TimeWidget;
-        case WidgetType.Date:
-            return DateWidget;
-        case WidgetType.Search:
-            return SearchWidget;
-    };
+enum WidgetType {
+    Text = "Text",
+    Time = "Time",
+    Date = "Date",
+    Search = "Search",
 };
 
-const widgets: Widget[] = Object.values(WidgetType).map(type => ({
-    type,
-    className: "widget-box",
-    content: createWidgets(type),
-}));
+enum WidgetCategory {
+    Text = "Text",
+    Time = "Time",
+    Date = "Date",
+    Search = "Search",
+    Weather = "Weather",
+    Music = "Music",
+};
+
+function createWidgets(type: WidgetType): { content: string; category: WidgetCategory } {
+    switch (type) {
+        case WidgetType.Text:
+            return { content: TextWidget, category: WidgetCategory.Text };
+        case WidgetType.Time:
+            return { content: TimeWidget, category: WidgetCategory.Time };
+        case WidgetType.Date:
+            return { content: DateWidget, category: WidgetCategory.Date };
+        case WidgetType.Search:
+            return { content: SearchWidget, category: WidgetCategory.Search };
+    }
+}
+
+const widgets: Widget[] = Object.values(WidgetType).map((type) => {
+    const { content, category: WidgetCategory } = createWidgets(type);
+
+    return {
+        type,
+        category: WidgetCategory,
+        className: 'widget-box',
+        content,
+    };
+});
 
 export { widgets };
