2aa57e0c18c180c27305238f4ce73f5c33253f91
BXn4 <bencegamer90@gmail.com>
2024-11-28 21:46:08 +0100

    Added more icons, created / load icons in utils

--- src/modules/widgets/components/filter.ts
+++ src/modules/widgets/components/filter.ts
@@ -1,5 +1,6 @@
 import { WidgetCategory } from "../widgetBuilder";
 import { getLocalizedText } from "../../../utils/locales"
+import { BasicIcons } from "../../../utils/icons";
 
 export function CategoryFilterWidget(category: WidgetCategory) {
   let menu_category_filter = document.getElementById("menu-category-filter");
@@ -12,20 +13,9 @@ export function CategoryFilterWidget(category: WidgetCategory) {
 
   menu_category_filter.textContent = getLocalizedText(category);
 
-  const namespace = "http://www.w3.org/2000/svg";
-  const icon = document.createElementNS(namespace, "svg");
-  icon.setAttribute("xmlns", namespace);
-  icon.setAttribute("viewBox", "0 0 512 512");
-  icon.setAttribute("width", "24");
-  icon.setAttribute("height", "24");
-
-  const path = document.createElementNS(namespace, "path");
-  path.setAttribute(
-    "d",
-    "M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"
-  );
-  icon.appendChild(path);
-
+  const icon = new DOMParser().parseFromString(BasicIcons.filter, 'image/svg+xml').documentElement;
+  icon.setAttribute('width', "24");
+  icon.setAttribute('height', "24");
   menu_category_filter.appendChild(icon);
 
   return menu_category_filter;
